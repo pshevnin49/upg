63f1d7e782dc3c831487150eb46a919529223fb7
pshev <pshev@10.20.10.12>
2021-03-06 21:56:52 +0100

    funguje asi dobre scalovani, spatne generuji souradnice zacatku sibek

--- UPG2021/src/DrawingPanel.java
+++ UPG2021/src/DrawingPanel.java
@@ -1,6 +1,10 @@
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
@@ -11,62 +15,76 @@ public class DrawingPanel extends JPanel {
 
 	static int maxColor = 0;
 	int[][] pixels = nacteni("data_plzen.pgm");
-
 	int width = pixels.length;
 	int height = pixels[0].length;
+	
 	int maxVyskaX = 0;
 	int maxVyskaY = 0;
 	int maxVyska = 10;
+	double x_min = 0;
+	double y_min = 0;
+	double x_max = width ;
+	double y_max = height;
 	
-//	int newWidth = (width * 2);
-//	int newHeight = (height * 2);
-//	int[][] newPixels = polace(pixels);
-
+	double world_width = x_max - x_min;
+	double world_height = y_max - y_min;
+	
 	public DrawingPanel() throws FileNotFoundException {
+		
+		
 		this.setPreferredSize(new Dimension(width, height));
 	}
 
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
-		System.out.println(width);
-		System.out.println(height);
+		Graphics2D g2 = (Graphics2D) g;
+
+		double scale_x = this.getWidth() / world_width;
+		double scale_y = this.getHeight() / world_height;
+		double scale = Math.min(scale_x, scale_y);
 		
-		if(width < 800 && height < 600) {
-			pixels = polace(pixels);
-			width = pixels.length;
-			height = pixels[0].length;
-		}
+//		System.out.println(width);
+//		System.out.println(height);
+
 		
 		for (int a = 0; a < height; a++) {//
 			for (int b = 0; b < width; b++) {
-				
-				if(pixels[b][a] > maxVyska) {
+
+				if (pixels[b][a] > maxVyska) {
 					maxVyska = pixels[b][a];
 					maxVyskaX = b;
 					maxVyskaY = a;
-					
+
 				}
 				if (maxColor > 255) {
 					double koef = maxColor / 255;
 					koef++;
+					
 					int color = (int) (pixels[b][a] / koef);
 					g.setColor(new Color(color, color, color));
-					g.fillRect(b, a, 1, 1);
+					g2.fill(new Rectangle2D.Double((b - x_min) * scale, (a - y_min) * scale, 1 * scale, 1 * scale));
 				} else if (maxColor < 255) {
 					double koef = 255 / maxColor;
 					koef++;
 					int color = (int) (pixels[b][a] * koef);
 					g.setColor(new Color(color, color, color));
-					g.fillRect(b, a, 1, 1);
+					g2.fill(new Rectangle2D.Double((b - x_min) * scale, (a - y_min) * scale, 1 * scale, 1 * scale));
 				} else {
 					g.setColor(new Color(pixels[b][a], pixels[b][a], pixels[b][a]));
-					g.fillRect(b, a, 1, 1);
+					g2.fill(new Rectangle2D.Double((b - x_min) * scale, (a - y_min) * scale, 1 * scale, 1 * scale));
 				}
 			}
 		}
-		g.drawLine(maxVyskaX, maxVyskaY, width/2, height/2);
-		System.out.println(maxVyska);
+		//g.drawLine(maxVyskaX, maxVyskaY, width / 2, height / 2);
+		drawArrow( (vypocetSourX(maxVyskaX, maxVyskaY)- x_min)*scale, (vypocetSourX(maxVyskaX, maxVyskaY)- y_min)*scale,(maxVyskaX - x_min)*scale, (maxVyskaY - y_min) * scale, 15, g2);
+		
+		
+		System.out.println("MinVyska: " + maxVyska);
+		System.out.println("X1 " + maxVyskaX );
+		System.out.println("Y1 " + maxVyskaY );
+		
+		//System.out.println(maxVyska);
 	}
 
 	public static int[][] nacteni(String jmenoSouboru) throws FileNotFoundException {
@@ -107,7 +125,7 @@ public class DrawingPanel extends JPanel {
 		return pixels;
 	}
 
-	public static int[][] polace(int[][] array) {
+	public static int[][] polace(int[][] array) { 
 		int height = array[0].length;
 		int width = array.length;
 		int newWidth = (width * 2);
@@ -147,5 +165,94 @@ public class DrawingPanel extends JPanel {
 		return newArray;
 
 	}
+	
+	private void drawArrow(double x1, double y1, double x2,double y2, double tip_length, Graphics2D g2) {
+        
+        double u_x = x2 - x1;
+        double u_y = y2 - y1;
+        double u_len1 = 1 / Math.sqrt(u_x * u_x + u_y*u_y);
+        u_x*= u_len1;
+        u_y*= u_len1;
+        
+        g2.setColor(Color.BLACK);
+        g2.setStroke(new BasicStroke(3));
+        
+        g2.draw(new Line2D.Double(x1,y1,x2,y2));
+        
+        double v_x = u_y;
+        double v_y = -u_x;
+        
+       
+        v_x *= 0.5 * tip_length;
+        v_y *= 0.5 * tip_length;
+        
+        double c_x =x2 - u_x*tip_length;
+        double c_y =y2 - u_y*tip_length;
+        
+        g2.setStroke(new BasicStroke(3));
+        g2.draw(new Line2D.Double(c_x + v_x,c_y + v_y,x2,y2));    
+        g2.draw(new Line2D.Double(c_x - v_x,c_y - v_y,x2,y2)); 
+	}
+	private double vypocetSourX(double x1, double y1) {
+		double x2 = 0;
+		double y2 = 0;
+		if(x1 >= width - 50) {
+			if(y1 >= height - 50) {
+				x2 = x1 - 25;
+			}
+			else if (y1 <= 0 + 50 ) {
+				x2 = x1 - 25;
+			}
+			else {
+				x2 = x1 - 50;
+			}
+		}
+		else if(x1 <= 0 + 50) {
+			if(y1 >= height - 50) {
+				x2 = x1 + 25;
+			}
+			else if (y1 <= 0 + 50 ) {
+				x2 = x1 + 25;
+			}
+			else {
+				x2 = x1 + 50;
+			}
+		}
+		else {
+			x2 = x1 - 25;
+		}
+		return x2;
+	}
+	private double vypocetSourY(double x1, double y1) {
+		double x2 = 0;
+		double y2 = 0;
+		if(y1 >= height - 50) {
+			if(y1 >= width - 50) {
+				y2 = y1 + 25;
+			}
+			else if (y1 <= 0 + 50 ) {
+				y2 = y1 + 25;
+			}
+			else {
+				y2 = y1 + 50;
+			}
+		}
+		else if(y1 <= 0 + 50) {
+			if(x1 >= width - 50) {
+				y2 = y1 - 25;
+			}
+			else if (x1 <= 0 + 50 ) {
+				y2 = y1 - 25;
+			}
+			else {
+				y2 = y1 - 50;
+			}
+		}
+		else {
+			y2 = y1 + 25;
+		}
+		return y2;
+	}
+	
 
 }
